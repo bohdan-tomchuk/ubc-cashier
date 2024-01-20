72c3830fd9902566c5597ba38cb76ec41285c52d
Bohdan Tomchuk <bogdan.tomchuck@gmail.com>
2024-01-20 16:39:26 +0200

    change routing to browser

--- client/src/router.tsx
+++ client/src/router.tsx
@@ -1,8 +1,8 @@
-import { createHashRouter } from 'react-router-dom'
+import { createBrowserRouter } from 'react-router-dom'
 import { Dashboard, Login, Cashier, Products } from './pages'
 import RootLayout from './layouts/RootLayout'
 
-export const router = createHashRouter([
+export const router = createBrowserRouter([
   {
     path: '/',
     element: <RootLayout />,
